4573dcef03fccd8c80072508c6132856e4764400
Thang Nguyen <nhthang90@gmail.com>
2013-04-13 22:55:51 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 # Script to provide a one line install for a Codecademy launcher for the Raspberry Pi.
 # 
@@ -9,21 +9,52 @@
 # Usage:
 # 
 # curl -L https://github.com/KanoComputing/codecademy-launcher/raw/master/install.sh | sh
-# wget --no-check-certificate https://github.com/KanoComputing/codecademy-launcher/raw/master/install.sh -O - | sh
+# wget --no-delete-certificate https://github.com/KanoComputing/codecademy-launcher/raw/master/install.sh -O - | sh
 
 LAUNCHER_DIR="/opt/kano/codecademy-launcher"
 
-if [ -d $LAUNCHER_DIR ]
+delete()
+{  
+	read key
+
+	case $key in
+	[yY][eE][sS]|[yY]|"")
+		sudo rm -rf $1
+		echo -e "\nDelete $1 complete!\n" 
+	;;
+	*)
+	exit
+	;;
+	esac
+}
+
+if test -d $LAUNCHER_DIR;
 then
-  echo "[Codecademy Launcher installer] It looks like you've already got the Codecademy Launcher installed."
-  echo "[Codecademy Launcher installer] You'll need to remove $LAUNCHER_DIR to install."
-  exit
+  	echo  -e "\n[Codecademy Launcher installer] It looks like you've already got the Codecademy Launcher installed."
+  	echo  -e "\n[Codecademy Launcher installer] You'll need to remove $LAUNCHER_DIR to install."
+  	echo  -e "\nWould you like to remove [Codecademy Launcher installer] directory[Y/n]?"
+	delete $LAUNCHER_DIR ;
 fi
 
-echo "[Codecademy Launcher installer] Installing Chromium."
-sudo apt-get -y install chromium git-core
+echo -e "[Codecademy Launcher installer] Installing Chromium ..."
+sudo apt-get -y install chromium-browser
 
-echo "[Codecademy Launcher installer] Installing desktop icon."
+echo  -e "\nInstalling git ...\n"
+sudo apt-get install git
+
+echo -e "\n[Codecademy Launcher installer] Installing desktop icon."
 sudo mkdir -p $LAUNCHER_DIR
 sudo git clone git://github.com/KanoComputing/codecademy-launcher.git $LAUNCHER_DIR
-ln -s $LAUNCHER_DIR/codecademy.desktop $HOME/Desktop/codecademy.desktop
+sudo cp $LAUNCHER_DIR/codecademy.desktop /usr/share/applications
+
+if test -e ~/Desktop/codecademy.desktop; 
+then
+	echo -e "\n~/Desktop/codecademy.desktop file has existes!\nDo you want to remove it[y/n]?"
+	delete ~/Desktop/codecademy.desktop;	
+fi
+	
+sudo ln -s $LAUNCHER_DIR/codecademy.desktop ~/Desktop/codecademy.desktop
+sudo chmod +x ~/Desktop/codecademy.desktop
+sudo chown $(whoami) ~/Desktop/codecademy.desktop
+sleep  2
+echo -e "\nComplete!\n"
